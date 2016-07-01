cea6a02e97a38788a285322388b797c4decb5a7c
Irfan Faiz <irfanfaiz734@gmail.com>
2016-07-02 06:49:46 +0700

    Create idul-fitri.js

---
+++ idul-fitri.js
@@ -0,0 +1,3 @@
+var target_date=new Date("Jul 06, 2015").getTime();var days,hours,minutes,seconds;var hitungmundur=document.getElementById("hitungmundur");setInterval(function(){var current_date=new Date().getTime();var seconds_left=(target_date- current_date)/ 1000;
+days=parseInt(seconds_left/86400);seconds_left=seconds_left%86400;hours=parseInt(seconds_left/3600);seconds_left=seconds_left%3600;minutes=parseInt(seconds_left/60);seconds=parseInt(seconds_left%60);hitungmundur.innerHTML=days+" <span class=\'digital\'>Hari</span> "+ hours+" <span class=\'digital\'>Jam</span> "
++ minutes+" <span class=\'digital\'>Menit</span> "+ seconds+" <span class=\'digital\'>Detik Menuju</span> <span class=\'idjudul\'>Idul Fitri 1437H / 2016</span>";},1000);
